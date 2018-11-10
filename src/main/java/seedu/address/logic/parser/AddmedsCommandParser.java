f5b7c32555be7efc0537da1cc86809f2e5dd6bc6
snajef <40816755+snajef@users.noreply.github.com>
2018-11-11 00:17:33 +0800

    Fix (?) trailing whitespace in AddmedsCommandParser.java Was unable to find this trailing whitespace, committing to force Travis to rebuild.

--- src/main/java/seedu/address/logic/parser/AddmedsCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddmedsCommandParser.java
@@ -39,7 +39,7 @@ public class AddmedsCommandParser implements Parser<AddmedsCommand> {
      * Parses the given {@code String} of arguments in the context of the
      * AddmedsCommand and returns an AddmedsCommand object for execution.
      *
-     * @throws ParseException if the user input does not conform to the expected format
+     * @throws ParseException if the user input does not conform to the expected format.
      */
     @Override
     public AddmedsCommand parse(String args) throws ParseException {
