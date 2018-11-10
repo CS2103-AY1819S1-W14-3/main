89d2feab1b565d6f7c3cb642c70d5fc3c6373c74
jeffypie369 <jffersonzzz@hotmail.com>
2018-11-10 15:34:31 +0800

    Add test for invalid nric for addappt

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -46,6 +46,7 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 public class CommandTestUtil {
     public static final String VALID_NRIC_AMY = "S0000100H";
     public static final String VALID_NRIC_BOB = "S0000100K";
+    public static final String INVALID_NRIC_BOB = "0000100K";
     public static final String VALID_NRIC_CHARLIE = "S0000100L";
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
@@ -80,6 +81,7 @@ public class CommandTestUtil {
 
     public static final String NRIC_DESC_AMY = " " + PREFIX_NRIC + VALID_NRIC_AMY;
     public static final String NRIC_DESC_BOB = " " + PREFIX_NRIC + VALID_NRIC_BOB;
+    public static final String INVALID_NRIC_DESC_BOB = " " + PREFIX_NRIC + INVALID_NRIC_BOB;
     public static final String NRIC_DESC_CHARLIE = " " + PREFIX_NRIC + VALID_NRIC_CHARLIE;
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
